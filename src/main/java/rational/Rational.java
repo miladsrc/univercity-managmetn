2d2e45c7046c6f3c6f1954881b059a0dab7256d5
milad <baranimilad70@yahoo.com>
2024-04-01 11:54:52 +0330

    #4-directory changed

--- src/main/java/rational/Rational.java
+++ src/main/java/rational/Rational.java
@@ -1,4 +1,4 @@
-package test;
+package rational;
 
 public class Rational {
 
--- src/test/java/rational/RationalTest.java
+++ src/test/java/rational/RationalTest.java
@@ -1,6 +1,5 @@
-package test;
+package rational;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
